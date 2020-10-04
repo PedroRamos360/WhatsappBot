43c5e120f9a51560fdf655ea6c3178b320a161ab
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-10-04 20:01:50 -0300

    Programação de data da mensagem e desligamento do computador após envio da mensagem

--- whatsapp_bot/main.py
+++ whatsapp_bot/main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+import time
+import os
 
 chromedriver = 'D:\Programas\ChromeDriver\chromedriver'
 driver = webdriver.Chrome(chromedriver)
@@ -6,6 +8,22 @@ driver.get('https://web.whatsapp.com/')
 
 name = input("Group name: ")
 message = input("Message: ")
+when = input("Execution date (day:hour:minute:second): ")
+
+day, hour, minute, second = when.split(":")
+
+day = int(day)
+hour = int(hour)
+minute = int(minute)
+second = int(second)
+
+now = time.localtime()
+now_in_seconds = now.tm_mday * 86400 + now.tm_hour * 3600 + now.tm_min * 60 + now.tm_sec
+date_in_seconds = day * 86400 + hour * 3600 + minute * 60 + second
+
+sleep_time = date_in_seconds - now_in_seconds
+
+time.sleep(sleep_time)
 
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
@@ -16,6 +34,7 @@ message_box.send_keys(message)
 button = driver.find_element_by_xpath("//button[@class='_1U1xa']")
 button.click()
 
+os.system("shutdown -s -t 60")
 
 
 
